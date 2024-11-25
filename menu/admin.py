54288e8a2400cb33f60b6929a653e82e296cccd4
Hazel Hawadi <hawadihazel@icloud.com>
2024-11-25 09:59:00 +0000

    Register MenuItem model in Django admin with search and filter capabilities

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MenuItem
 
 # Register your models here.
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category')
+    search_fields = ('name', 'category')
+    list_filter = ('category',)
