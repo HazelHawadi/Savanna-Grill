639056c4da67bf86adea95b10db022c8257a7552
Hazel Hawadi <hawadihazel@icloud.com>
2024-11-27 18:22:03 +0000

    Add  Submit review view

--- savanna_grill/views.py
+++ savanna_grill/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Review  
 
 def home(request):
     return render(request, 'home.html')
+
+# Submit review view
+def submit_review(request):
+    if request.method == 'POST':
+        review_text = request.POST.get('review')
+        rating = request.POST.get('rating')  
+
+        return redirect('home')
+
+return HttpResponse("Invalid request", status=400)
